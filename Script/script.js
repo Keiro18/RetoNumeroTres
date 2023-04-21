58fe57d70cddfbe5ae8a22f1c578ef5bc872e443
Juan Dario Malagon <j.malagon@utp.edu.co>
2023-04-21 02:34:01 -0500

    se cambia fondo y logica

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+import datos from '../Data/datos.json'assert { type: "json" };
+const comenzar = document.getElementById('comenzar');
+
+console.log(datos);
+
+comenzar.addEventListener('click', (e) => {
+    let id = getElementById(datos).value;
+
+});
